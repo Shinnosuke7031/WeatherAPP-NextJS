5e8f0fc3759dc7b954d8bf9330c4ad5442a93cb8
飯野信之介 <iino@iinoMacBookPro.local>
2020-07-25 21:32:44 +0900

    Add <style jsx>

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,24 @@ const Header = () => (
         <Link href="/weather">
           <a style={linkStyle}>Weather</a>
         </Link>
+        <style jsx>{`
+        div {
+          width: 100%;
+          height: 50px;
+          background-color: beige;
+          padding: 0;
+          margin: 0;
+          display: flex;
+          justify-content: center;
+        }
+
+        a {
+          display: inline-block;
+          text-align: center;
+          font-size: 30px;
+          width: 200px;
+        }
+        `}</style>
     </div>
 )
 
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -8,10 +8,18 @@ const layoutStyle = {
 }
 
 const Layout = (props) => (
-  <div style={layoutStyle}>
+  <div>
     <Header />
     {props.children}
     <Footer />
+    <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
+                       "Hiragino Sans", Meiryo, sans-serif; 
+        }
+      `}</style>
   </div>
 )
 
